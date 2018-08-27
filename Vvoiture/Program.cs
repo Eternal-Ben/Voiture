cdc5a3cceda134071cbaf26a0c5234bd90a7d139
Tliseron <lst.thierry@gmail.com>
2018-08-27 14:29:12 +0200

    Ajout de var chaineConnexion = ConfigurationManager.ConnectionStrings["ConnexionDB"].ConnectionString; return new.SqlConnection(chaineConnexion); return new SqlConnection;
    Modification du fichier config pour permetre une connecion à la base de donnée.

--- Vvoiture/Program.cs
+++ Vvoiture/Program.cs
@@ -3,13 +3,309 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Configuration;
 
-namespace Vvoiture
+
+namespace Voitures
+
 {
+
     class Program
+
     {
+
         static void Main(string[] args)
+
         {
+
+            Console.WriteLine("VOITURES");
+
+            Console.WriteLine();
+
+
+
+            while (true)
+
+            {
+
+                var choix = AfficherMenu();
+
+                switch (choix)
+
+                {
+
+                    case 1:
+
+                        int idMarque = ChoisirMarque();
+
+                        AfficherModeles(idMarque);
+
+                        break;
+
+
+
+                    case 2:
+
+                        AfficherMarques();
+
+                        break;
+
+
+
+                    case 3:
+
+                        CreerMarque();
+
+                        break;
+
+
+
+                    case 4:
+
+                        SupprimerMarque();
+
+                        break;
+
+
+
+                    case 9:
+
+                        Environment.Exit(0);
+
+                        break;
+
+                }
+
+
+
+                Console.WriteLine("Appuyez pour retourner au menu...");
+
+                Console.ReadKey();
+
+            }
+
         }
+
+
+
+        private static int ChoisirMarque()
+
+        {
+
+            AfficherMarques();
+
+
+
+            Console.WriteLine("Quelle marque (Id)?");
+
+            return int.Parse(Console.ReadLine());
+
+        }
+
+
+
+        private static void AfficherMarques()
+
+        {
+
+            Console.WriteLine();
+
+            Console.WriteLine("> MARQUES");
+
+
+
+            var connexion = CreerConnexion();
+
+            connexion.Open();
+
+
+
+            var commande = connexion.CreateCommand();
+
+            commande.CommandText = "SELECT * FROM Marques ORDER BY Nom";
+
+
+
+            var dataReader = commande.ExecuteReader();
+
+            while (dataReader.Read())
+
+            {
+
+                var indexColonneNom = dataReader.GetOrdinal("Nom");
+
+                var indexColonneId = dataReader.GetOrdinal("Id");
+
+                Console.Write(dataReader.GetString(indexColonneNom));
+
+                Console.Write(" (");
+
+                Console.Write(dataReader.GetInt32(indexColonneId));
+
+                Console.WriteLine(")");
+
+            }
+
+
+
+            connexion.Close();
+
+        }
+
+
+
+        private static void AfficherModeles(int idMarque)
+
+        {
+
+            Console.WriteLine();
+
+            Console.WriteLine("> MODELES");
+
+
+
+            var connexion = CreerConnexion();
+
+            connexion.Open();
+
+
+
+            var commande = connexion.CreateCommand();
+
+            commande.CommandText =
+
+                @"SELECT M.Nom AS NomModele, S.Nom AS NomSegment
+
+                  FROM Modeles M
+
+                      INNER JOIN Segments S ON S.Id = M.IdSegment
+
+                  WHERE IdMarque = @IdMarque";
+
+            commande.Parameters.AddWithValue("@IdMarque", idMarque);
+
+
+
+            var dataReader = commande.ExecuteReader();
+
+            while (dataReader.Read())
+
+            {
+
+                var indexColonneNomModele = dataReader.GetOrdinal("NomModele");
+
+                var indexColonneNomSegment = dataReader.GetOrdinal("NomSegment");
+
+                Console.Write(dataReader.GetString(indexColonneNomModele));
+
+                Console.Write(" (");
+
+                Console.Write(dataReader.GetString(indexColonneNomSegment));
+
+                Console.WriteLine(")");
+
+            }
+
+
+
+            connexion.Close();
+
+        }
+
+
+
+        private static void CreerMarque()
+
+        {
+
+            Console.WriteLine();
+
+            Console.WriteLine(">NOUVELLE MARQUE");
+
+
+
+            Console.Write("Nom de la marque: ");
+
+            var nomMarque = Console.ReadLine();
+
+
+
+            var connexion = CreerConnexion();
+
+            connexion.Open();
+
+
+
+            var commande = connexion.CreateCommand();
+
+            commande.CommandText =
+
+                "INSERT INTO Marques (Nom) VALUES(@NomMarque)";
+
+            commande.Parameters.AddWithValue("@NomMarque", nomMarque);
+
+
+
+            commande.ExecuteNonQuery();
+
+
+
+            connexion.Close();
+
+        }
+
+
+
+        private static void SupprimerMarque()
+
+        {
+
+
+
+        }
+
+
+
+        private static int AfficherMenu()
+
+        {
+
+            Console.Clear();
+
+
+
+            Console.WriteLine("1. Afficher les modèles");
+
+            Console.WriteLine("2. Afficher les marques");
+
+            Console.WriteLine("3. Créer une marque");
+
+            Console.WriteLine("4. Supprimer une marque");
+
+            Console.WriteLine("9. Quitter");
+
+
+
+            Console.Write("Votre choix: ");
+
+            return int.Parse(Console.ReadLine());
+
+        }
+
+
+
+        static SqlConnection CreerConnexion()
+
+        {
+
+            var chaineConnexion = ConfigurationManager.ConnectionStrings["ConnexionDB"].ConnectionString;
+
+            return new SqlConnection(chaineConnexion);
+
+        }
+
     }
+
 }
